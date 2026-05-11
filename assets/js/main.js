9d9491765fc23b702483093845e31b2ae1f924a8
wilty <71175061+wiliantiwilty@users.noreply.github.com>
2026-05-11 15:26:14 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,7 @@
    */
   const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
 
-  function mobileNavToogle() {
+  /*function mobileNavToogle() {
     document.querySelector('body').classList.toggle('mobile-nav-active');
     mobileNavToggleBtn.classList.toggle('bi-list');
     mobileNavToggleBtn.classList.toggle('bi-x');
